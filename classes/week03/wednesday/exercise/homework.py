6eb28ab6087ae45612d671372f6903f830b299e4
Bob Slotpole <rslotpole@babson.edu>
2025-09-14 21:33:51 +0000

    feat: create homework.py

---
+++ classes/week03/wednesday/exercise/homework.py
@@ -0,0 +1,9 @@
+'''
+please prepare two questions you may have on the tictactoe lab  --> labs/lab2/Lab 02.pdf 
+and or run tictactoe.py in labs/lab2/student_code to see how the game plays -> but come 
+up with two questions/
+
+Also, please prepare three questions you have on Python Notes Part 1 or Part 2 (pdfs)
+-> week03/
+
+'''
