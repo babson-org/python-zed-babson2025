8b56cecbd9501175c98570e2d9c9bdc8cb6f4458
Bob Slotpole <rslotpole@babson.edu>
2025-09-24 19:45:07 +0000

    update 6 files

--- portfolio3/update/_update_position.py
+++ portfolio3/update/_update_position.py
@@ -15,7 +15,35 @@ def _update_position(active_client, transaction):
 
             active_client['positions'].append(position)    # or client['positions].append(position)
     elif transaction['type'] == 'BUY':
-        pass
+        current_position = None
+        for position in active_client['positions']:            
+            if transaction['symbol'] == position['symbol']:                
+                current_position = position
+                break
+
+        if current_position:
+            
+            old_total_cost = current_position['shares'] * current_position['avg_cost']
+            new_total_cost = old_total_cost + transaction['shares'] * transaction['trn_price']
+            
+            current_position['shares'] += transaction['shares']
+            current_position['avg_cost'] = new_total_cost / current_position['shares']
+            
+        else:
+            position = {'id': transaction['id'],            
+            'shares': round(transaction['shares'], 2),
+            'symbol': transaction['symbol'],
+            'name': transaction['name'],
+            'avg_cost': transaction['trn_price']}
+            active_client['positions'].append(position) 
+
+        for position in active_client['positions']:
+            if '$$$$' == position['symbol']:
+                position['shares'] -= transaction['shares'] * transaction['trn_price']
+
+
+
+        
     elif transaction['type'] == 'SELL':
         pass
     elif transaction['type'] == 'WITHDRAWAL':
