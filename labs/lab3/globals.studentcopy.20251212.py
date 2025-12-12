b5c4a0b907d35768932bb93b5b0d12eedcf3cba6
student-name <student@example.com>
2025-12-12 16:00:09 -0500

    Sync from class repo (20251212)

---
+++ labs/lab3/globals.studentcopy.20251212.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+ROWS = None
+COLS = None
+MINES = None
+=======
+ROWS = None
+COLS = None
+MINES = None
+>>>>>>> d3673045b533568b8e7bb35cd49bf2b6d633bc1d
---
+++ labs/lab3/utils.studentcopy.20251212.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+import os
+
+def clear_screen():
+    """Clears the terminal screen."""
+=======
+import os
+
+def clear_screen():
+    """Clears the terminal screen."""
+>>>>>>> d3673045b533568b8e7bb35cd49bf2b6d633bc1d
+    os.system('cls' if os.name == 'nt' else 'clear')
